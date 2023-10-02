6e1dc129d3db03b2c54e13f27120e71db6866550
Bjørn Fjukstad <bjornfjukstad@gmail.com>
2023-10-02 08:58:59 +0200

    centering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex bg-white dark:bg-[#171616] text-gray-90 dark:text-white h-screen font-sans" >
-        <main className="grid grid-flow-ros auto-rows-max w-full flex py-12 ">
+      <body className="flex bg-white dark:bg-[#171616] text-gray-90 dark:text-white h-screen font-sans justify-center" >
+        <main className="mx-auto flex flex-col items-center py-12 w-full md:max-w-screen-xl ">
           <div className="grid grid-cols-12 w-full h-12">
             <div className="col-start-2 col-span-10 md:col-span-3 md:col-start-4">
               <div className="flex space-x-4">
