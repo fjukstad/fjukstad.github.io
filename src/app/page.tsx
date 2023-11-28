9cf452c6738f7eb140e09b773cd39b655a3a7c5c
Bjørn Fjukstad <bjornfjukstad@gmail.com>
2023-11-28 11:54:13 +0100

    bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div className="text-gray-600 dark:text-gray-300 font-light">
       <p className="pb-4 font-bold"> Hey, I&apos;m Bjørn! </p>
       <p className=""> I&apos;m a product manager at <Link className="font-bold" href="https://www.dips.com" target="_blank">DIPS</Link>,
-        currently buidling our developer platform to enable everyone
-        to collaborate on the future of Norwegian health care.</p>
+        currently focusing on enabling everyone
+        to collaborate on building the future of Norwegian health care.</p>
 
       <div className="grid grid-cols-3 gap-4 min-w-1/2 py-6 items-start">
         <div className="flex flex-col">
@@ -22,10 +22,9 @@ export default function Home() {
         </div>
       </div>
 
-      <p className="py-4"> I have a background in Computer Science with a PhD from the
+      <p className="py-4"> I have a PhD in Computer Science from the
         <Link className="font-bold" target="_blank" href="https://uit-hdl.github.io/"> Health Data Lab </Link>
         at the Arctic University of Norway in Tromsø.
-
       </p>
 
       <div className="py-6">
